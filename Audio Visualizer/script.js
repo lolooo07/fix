cec986d2ef9d895afbab203023255094143cac15
lolooo07 <lolooo07@users.noreply.github.com>
2025-07-07 00:00:00 +0000

    add small feature improvements

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -229,4 +229,8 @@ let audioCtx;
 let analyser;
 let source;
 let isPlaying = false;
-
+
+// Function to set up the Web Audio API
+function setupAudioContext() {
+    if (!audioCtx) {
+        audioCtx = new (window.AudioContext || window.webkitAudioContext)();
