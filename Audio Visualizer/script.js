62efb5e4a4a41bf94e2c81edbefb57e94097e01f
lolooo07 <lolooo07@users.noreply.github.com>
2025-07-12 00:00:00 +0000

    adjust project settings for stability

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -701,4 +701,6 @@ audioUpload.addEventListener('change', (e) => {
         setupAudioContext();
     }
 });
-
+
+// Function to play or pause the audio
+playPauseBtn.addEventListener('click', () => {
