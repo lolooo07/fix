d4b1a128fb9346d854c68314131f59d943e14c50
lolooo07 <lolooo07@users.noreply.github.com>
2025-07-20 00:00:00 +0000

    general maintenance and updates

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -337,4 +337,8 @@ const canvasCtx = visualizerCanvas.getContext('2d');
 // --- Web Audio API Components ---
 let audioCtx;
 let analyser;
-let source;
+let source;
+let isPlaying = false;
+
+// Function to set up the Web Audio API
+function setupAudioContext() {
