eda8d93da6bb10ba9f8e1d13f43764f6c1e3c11e
lolooo07 <lolooo07@users.noreply.github.com>
2025-07-21 00:00:00 +0000

    small tweaks and code enhancements

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -353,4 +353,8 @@ function setupAudioContext() {
         
         // Set up the analyser for frequency data
         analyser.fftSize = 256;
-        const bufferLength = analyser.frequencyBinCount;
+        const bufferLength = analyser.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
+        
+        // Start the visualization loop
+        drawVisualizer(bufferLength, dataArray);
