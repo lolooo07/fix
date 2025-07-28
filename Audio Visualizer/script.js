59ec37b7138f326186a296efcea6b8d9bbf0c6bb
lolooo07 <lolooo07@users.noreply.github.com>
2025-07-28 00:00:00 +0000

    refactor: split code into smaller functions

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -401,4 +401,8 @@ function drawVisualizer(bufferLength, dataArray) {
     // Request the next frame
     requestAnimationFrame(() => drawVisualizer(bufferLength, dataArray));
     
-    // Get the frequency data
+    // Get the frequency data
+    analyser.getByteFrequencyData(dataArray);
+
+    const barWidth = (visualizerCanvas.width / bufferLength) * 2;
+    let x = 0;
